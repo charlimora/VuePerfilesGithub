5f86bae83de852bfce274d36dffb39c3e3f977cc
charlimora <ca.agudelo.2021@gmail.com>
2022-05-18 18:25:40 -0500

    Trabajando con primeros eventos y modelos

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 const API = "https://api.github.com/users/";
 
-async function Buscar(){
-    const response = await fetch(API + 'charlimora')
-    const data = await response.json()
-    console.log(data)
-}
-
 const app = Vue.createApp({
     data() {
         return {
-          message: 'Hello Vue!'
+          busqueda:null
         }
+      },
+      methods: {
+          async Buscar(){
+          const response = await fetch(API + 'charlimora')
+          const data = await response.json()
+          console.log(data)
+      }
       }
 })
