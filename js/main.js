99af5c0069e5c7b10516147c1b77cd2c4ae2b6f4
charlimora <ca.agudelo.2021@gmail.com>
2022-05-19 12:07:31 -0500

    Añadir funcionalidad a botón busqueda

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ const API = "https://api.github.com/users/";
 const app = Vue.createApp({
     data() {
         return {
-          busqueda:null
-        }
+          busqueda: null
+        };
       },
       methods: {
           async Buscar(){
-          const response = await fetch(API + 'charlimora')
+          const response = await fetch(API + this.busqueda)
           const data = await response.json()
           console.log(data)
       }
       }
-})
+});
