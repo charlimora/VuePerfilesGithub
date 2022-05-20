861243a1dbbe23dbacf94b34bf528d4518d25aa6
charlimora <ca.agudelo.2021@gmail.com>
2022-05-19 19:06:57 -0500

    Mostrando y ocultando el div result segun busqueda

--- js/main.js
+++ js/main.js
@@ -3,14 +3,27 @@ const API = "https://api.github.com/users/";
 const app = Vue.createApp({
     data() {
         return {
-          busqueda: null
+          busqueda: null,
+          result: null,
+          error: null
         };
       },
       methods: {
           async Buscar(){
-          const response = await fetch(API + this.busqueda)
-          const data = await response.json()
-          console.log(data)
+
+          try {
+            const response = await fetch(API + this.busqueda)
+            if(!response.ok) throw new Error("Usuario no encontrado")
+            //console.log(response)
+            const data = await response.json()
+            console.log(data)
+            this.result = true
+            
+          } catch (error) {
+            this.error = error
+          } finally {
+            this.busqueda = null
+          }
       }
       }
 });
