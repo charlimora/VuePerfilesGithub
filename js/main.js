97994eaab9924e8be87d2237f88f78821c450df6
charlimora <ca.agudelo.2021@gmail.com>
2022-05-20 16:41:54 -0500

    Mostrar info del usuario en widget

--- js/main.js
+++ js/main.js
@@ -10,14 +10,16 @@ const app = Vue.createApp({
       },
       methods: {
           async Buscar(){
-
+          //con la siguiente línea empezamos limpiando todo
+          this.result = this.error = null
           try {
             const response = await fetch(API + this.busqueda)
             if(!response.ok) throw new Error("Usuario no encontrado")
             //console.log(response)
             const data = await response.json()
             console.log(data)
-            this.result = true
+          //hacemos que result en vez que dé un true, arroje el data
+            this.result = data
             
           } catch (error) {
             this.error = error
